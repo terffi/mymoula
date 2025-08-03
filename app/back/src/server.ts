28fa81a75933a69ac804feaa0049baf11d1fcdc3
justsam^^ <sam54670@hotmail.com>
2025-08-04 01:22:13 +0200

    i m so tired

--- app/back/src/server.ts
+++ app/back/src/server.ts
@@ -8,61 +8,58 @@ import ViteExpress from 'vite-express';
 
 import { appRouter } from '@api/router';
 
-async function main() {
-  const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-  const app = express();
+const app = express();
 
-  app.use(cors());
+app.use(cors());
 
-  app.use(
-    '/trpc',
-    createExpressMiddleware({
-      router: appRouter,
-      createContext: (ctx) => ({ session: null }),
-      onError:
-        process.env.NODE_ENV === 'development'
-          ? ({ path, error }) => {
-              console.error(
-                `❌ tRPC failed on ${path ?? '<no-path>'}: ${error.message}`
-              );
-            }
-          : undefined,
-    })
-  );
+app.use(
+  '/trpc',
+  createExpressMiddleware({
+    router: appRouter,
+    createContext: (ctx) => ({ session: null }),
+    onError:
+      process.env.NODE_ENV === 'development'
+        ? ({ path, error }) => {
+            console.error(
+              `❌ tRPC failed on ${path ?? '<no-path>'}: ${error.message}`
+            );
+          }
+        : undefined,
+  })
+);
 
-  ViteExpress.config({
-    mode: 'production',
-    inlineViteConfig: {
-      build: { outDir: 'app/front/dist' },
-    },
-  });
+ViteExpress.config({
+  mode: 'production',
+  inlineViteConfig: {
+    build: { outDir: 'app/front/dist' },
+  },
+});
 
-  ViteExpress.listen(app, 3000, () => console.log('Server is listening...'));
+ViteExpress.listen(app, 3000, () => console.log('Server is listening...'));
 
-  // const __filename = fileURLToPath(import.meta.url);
-  // const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-  // // Make sure to put this after all api routes are being handled (e.g. app.use('/api/authorize', authRoutes);)
-  // console.log(process.env.NODE_ENV)
-  // console.log(__dirname)
-  // if (process.env.NODE_ENV === "production") {
-  //   app.use(express.static(path.join(__dirname, "../../front/dist")));
+// // Make sure to put this after all api routes are being handled (e.g. app.use('/api/authorize', authRoutes);)
+// console.log(process.env.NODE_ENV)
+// console.log(__dirname)
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../../front/dist")));
 
-  //   app.get("*", (req, res) => {
-  //     return res.sendFile(path.join(__dirname, "../../front/dist/index.html"));
-  //   });
-  // }
+//   app.get("*", (req, res) => {
+//     return res.sendFile(path.join(__dirname, "../../front/dist/index.html"));
+//   });
+// }
 
-  // For testing purposes, wait-on requests '/'
-  // app.get('/', (req, res) => res.send('Server is running!'));
+// For testing purposes, wait-on requests '/'
+// app.get('/', (req, res) => res.send('Server is running!'));
 
-  // app.listen(port, () => {
-  //   console.log(`App listening on port: ${port}`);
-  // });
-}
+// app.listen(port, () => {
+//   console.log(`App listening on port: ${port}`);
+// });
 
-void main();
 // import { createHTTPServer } from '@trpc/server/adapters/standalone';
 // import { appRouter } from './router';
 // import cors from 'cors';
@@ -99,3 +96,5 @@ void main();
 // }
 
 // server.listen(3000);
+
+export default app;
