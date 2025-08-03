732fbb6fb3bcaea1569a6912b546cd80a528bdc0
justsam^^ <sam54670@hotmail.com>
2025-08-03 20:40:06 +0200

    use express for backend + express serve static files

--- app/back/src/server.ts
+++ app/back/src/server.ts
@@ -1,23 +1,92 @@
-import { createHTTPServer } from '@trpc/server/adapters/standalone';
-import { appRouter } from './router';
+import { createExpressMiddleware } from '@trpc/server/adapters/express';
 import cors from 'cors';
+import 'dotenv/config';
+import express from 'express';
+import path from "path";
+// import { fileURLToPath } from "url";
 
-// const port = process.env.PORT || 3000;
+import { appRouter } from '@api/router';
 
 
-const server = createHTTPServer({
-  middleware: cors(),
-  router: appRouter,
-  createContext() {
-    return { session: null };
-  },
-  basePath: '/trpc/',
-  // onError:
-  //       process.env.NODE_ENV === 'development'
-  //         ? ({ path, error }) => {
-  //             console.error(`❌ tRPC failed on ${path ?? '<no-path>'}: ${error.message}`);
-  //           }
-  //         : undefined,
+async function main() {
+  const port = process.env.PORT || 3000;
+
+  const app = express();
+
+  app.use(cors());
+
+
+  app.use(
+    '/trpc',
+    createExpressMiddleware({
+      router: appRouter,
+      createContext: (ctx) => ({ session: null }),
+      onError:
+        process.env.NODE_ENV === 'development'
+          ? ({ path, error }) => {
+              console.error(`❌ tRPC failed on ${path ?? '<no-path>'}: ${error.message}`);
+            }
+          : undefined,
     })
+  );
 
-server.listen(3000);
+  // const __filename = fileURLToPath(import.meta.url);
+  // const __dirname = path.dirname(__filename);
+
+  // Make sure to put this after all api routes are being handled (e.g. app.use('/api/authorize', authRoutes);)
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
+
+    app.get("*", (req, res) => {
+      return res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+    });
+  }
+
+  // For testing purposes, wait-on requests '/'
+  app.get('/', (req, res) => res.send('Server is running!'));
+
+  app.listen(port, () => {
+    console.log(`App listening on port: ${port}`);
+  });
+}
+
+
+void main();
+// import { createHTTPServer } from '@trpc/server/adapters/standalone';
+// import { appRouter } from './router';
+// import cors from 'cors';
+// import { fileURLToPath } from 'url';
+// import path from 'path';
+
+// // const port = process.env.PORT || 3000;
+
+
+// const server = createHTTPServer({
+//   middleware: cors(),
+//   router: appRouter,
+//   createContext() {
+//     return { session: null };
+//   },
+//   basePath: '/trpc/',
+//   // onError:
+//   //       process.env.NODE_ENV === 'development'
+//   //         ? ({ path, error }) => {
+//   //             console.error(`❌ tRPC failed on ${path ?? '<no-path>'}: ${error.message}`);
+//   //           }
+//   //         : undefined,
+//     })
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// // Make sure to put this after all api routes are being handled (e.g. app.use('/api/authorize', authRoutes);)
+// if (process.env.NODE_ENV === "production") {
+//   server.use(express.static(path.join(__dirname, "../client/dist")));
+
+//   server.get("*", (req, res) => {
+//     return res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+//   });
+// }
+
+// server.listen(3000);
+
