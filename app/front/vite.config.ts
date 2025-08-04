0dc218a31667f543f50ac84f9ddbdd3dbe6e369d
justsam^^ <sam54670@hotmail.com>
2025-08-04 02:07:58 +0200

    maybe?

--- app/front/vite.config.ts
+++ app/front/vite.config.ts
@@ -5,5 +5,13 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),tsconfigPaths()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  server: {
+    proxy: {
+      '/api': {
+        target: `http://localhost:3000`,
+        changeOrigin: true,
+      },
+    },
+  },
 });
